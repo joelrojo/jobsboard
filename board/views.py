b9d72cd28885dc52c1ed98b1405c3dfb59424d63
Joel Rojo <joel.a.rojo@gmail.com>
2012-07-18 09:55:13 -0700

    got ajax submit to work

--- board/views.py
+++ board/views.py
@@ -3,18 +3,20 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.conf import settings
 import requests
+import json
 
 # Create your views here.
 def home(request, template='board/home.html'):
     c = {}
     return render_to_response(template, c, context_instance=RequestContext(request))
 
-def search(request, template='board/home.html'):
+def search(request):
     c = {}
     query = request.GET.get('query', '')
     location = request.GET.get('location', '')
     payload = {'publisher': '7575596370066104', 'v': '2', 'format': 'json', 'q': query, 'l': location}
     r = requests.get('http://api.indeed.com/ads/apisearch', params=payload)
-    c['results'] = r.text
-    return render_to_response(template, c, context_instance=RequestContext(request))
+    c['success'] = 'true'
+    c['result'] = r.text
+    return HttpResponse(json.dumps(c), mimetype="application/json")
     
