418fa246e236b10ea7752cdbc88092ff62c4715e
Joel Rojo <joel.a.rojo@gmail.com>
2012-07-18 00:15:44 -0700

    working on ajax results pushing

--- board/views.py
+++ board/views.py
@@ -9,14 +9,12 @@ def home(request, template='board/home.html'):
     c = {}
     return render_to_response(template, c, context_instance=RequestContext(request))
 
-def search(request, template='board/search.html'):
+def search(request, template='board/home.html'):
     c = {}
-    query = request.GET.get('query')
-    query = query if query else ""
-    location = request.GET.get('location')
-    location = location if location else ""
+    query = request.GET.get('query', '')
+    location = request.GET.get('location', '')
     payload = {'publisher': '7575596370066104', 'v': '2', 'format': 'json', 'q': query, 'l': location}
     r = requests.get('http://api.indeed.com/ads/apisearch', params=payload)
-    c['results'] = r.json
+    c['results'] = r.text
     return render_to_response(template, c, context_instance=RequestContext(request))
     
